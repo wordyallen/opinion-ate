160c51c729d8b094d10f033cf0d0b2c802a27abe
resister <iot.resister@protonmail.com>
2021-06-27 18:17:30 -0500

    Specificed listing restaurants

---
+++ cypress/integration/restaurants.test.js
@@ -0,0 +1,19 @@
+describe('Listing Restaurants', () => {
+  it('shows restaurants from the server', () => {
+    const sushiPlace = 'Sushi Place';
+    const pizzaPlace = 'Pizza Place';
+    cy.server({force404: true});
+    cy.route({
+      method: 'GET',
+      url: `https://outside-in-dev-api.herokuapp.com/${process.env.API_KEY}/restaurants`,
+      response: [
+        {id: 1, name: sushiPlace},
+        {id: 2, name: pizzaPlace},
+      ],
+    });
+
+    cy.visit('/');
+    cy.contains(sushiPlace);
+    cy.contains(pizzaPlace);
+  });
+});
